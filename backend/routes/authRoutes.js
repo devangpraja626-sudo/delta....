dda7cc089c40cbe1358019011da12a771a7a68c4
devangpraja626-sudo <devangpraja626@gmail.com>
2026-09-03 22:03:00 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,11 +8,15 @@ const {
 const router = express.Router();
 
 
+// ================= REGISTER =================
+
 router.post(
     "/register",
     register
 );
 
+
+// ================= LOGIN =================
 
 router.post(
     "/login",
