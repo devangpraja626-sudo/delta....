b25b7a4e79135be05dfd7f5d1eb6cda0ebff604b
devangpraja626-sudo <devangpraja626@gmail.com>
2026-09-03 22:04:12 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
+
+        if (!process.env.MONGO_URI) {
+            throw new Error(
+                "MONGO_URI is not defined"
+            );
+        }
+
         const connection = await mongoose.connect(
             process.env.MONGO_URI
         );
