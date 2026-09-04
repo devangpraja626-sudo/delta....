2060125e443335ff82a46f0ba9b0a7af71139399
devangpraja626-sudo <devangpraja626@gmail.com>
2026-09-05 02:11:13 +0530

    Create Message.js

---
+++ backend/models/Message.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+    {
+        conversation: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Conversation",
+            required: true
+        },
+
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        content: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 5000
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model(
+    "Message",
+    messageSchema
+);
