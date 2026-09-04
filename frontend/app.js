4d07ea19821f1a2aaea8fbfa18a601fd1930f66e
devangpraja626-sudo <devangpraja626@gmail.com>
2026-09-05 03:01:00 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -2,7 +2,7 @@
    DELTA FRONTEND JAVASCRIPT
 ========================================================= */
 
-const API_URL = "https://delta-6.onrender.com";
+const API_URL = "https://delta-7.onrender.com";
 
 /* =========================================================
    DOM ELEMENTS
